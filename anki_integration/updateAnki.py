47d9eaf91e9475c358f06baae4150fdac72558ff
dangnt0106 <bobodang4@gmail.com>
2025-08-06 22:14:21 +0700

    Revert "Update file date 0803"
    This reverts commit fc8a48ba3e8bbcc0aac7394d18559de9c1eb28b9.

--- anki_integration/updateAnki.py
+++ anki_integration/updateAnki.py
@@ -1,24 +1,32 @@
 import csv
-import os
-import sys
-import shutil
+import json
+import urllib.request
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from callAnki import request, invoke
+def request(action, **params):
+    return {'action': action, 'params': params, 'version': 6}
+
+def invoke(action, **params):
+    requestJson = json.dumps(request(action, **params)).encode('utf-8')
+    response = json.load(urllib.request.urlopen(urllib.request.Request('http://127.0.0.1:8765', requestJson)))
+    if len(response) != 2:
+        raise Exception('response has an unexpected number of fields')
+    if 'error' not in response:
+        raise Exception('response is missing required error field')
+    if 'result' not in response:
+        raise Exception('response is missing required result field')
+    if response['error'] is not None:
+        raise Exception(response['error'])
+    return response['result']
 
 # Đọc dữ liệu từ file CSV với dấu phân cách là semicolon
-csv_file = r"F:/studyingJapanese/csv/tuvung_0718.csv"
+csv_file = r"F:\studyingJapanese\csv\NguPhapN4_VD.csv"
 
 count_update = 0
 count_new = 0
 count_delete = 0
-desk = "TEST1"
-tags = ["TUVUNG"]
+desk = "NguPhap_Japanese"
+tags = ["N4", "grammar"]
 
-
-src = r"F:\\Japanese\\kaiwa\\audio\\自己紹介をしてください.mp3"
-dst = r"C:\\Users\\ADMIN\\AppData\\Roaming\\Anki2\\Người dùng 1\\collection.media\\自己紹介をしてください.mp3"
-shutil.copy(src, dst)
 
 with open(csv_file, mode='r', encoding='utf-8') as csvfile:
     reader = csv.reader(csvfile, delimiter=';')
