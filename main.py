fee2a3ac083102d3d7779a86504c66a6d3e63acb
DangNTD <113514281+dangnt0106@users.noreply.github.com>
2025-08-10 21:49:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from gui.app import app   # <-- import đúng biến app, không phải module
 
 if __name__ == "__main__":
-    app.launch(server_port=10000, server_name="127.0.0.1")
+    app.launch(server_port=10000, server_name="0.0.0.0")
